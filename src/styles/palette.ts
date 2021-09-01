afa7d600e72933d053015cd6f2482798efa945bf
3kingz <victorkenneth95@gmail.com>
2021-09-01 02:55:57 +0100

    initial commit

---
+++ src/styles/palette.ts
@@ -0,0 +1,13 @@
+export const palette = {
+  black: "#252625",
+  white: "#fcfcfc",
+  lightGrey: "#3b3b3b",
+  blackTextColor: "#31341d",
+  noteColor1: "#ffab91",
+  noteColor2: "#ffcc80",
+  noteColor3: "#e7ed9b",
+  noteColor4: "#81deea",
+  noteColor5: "#cf94da",
+  noteColor6: "#7fcbc3",
+  noteColor7: "#f48fb1",
+};
